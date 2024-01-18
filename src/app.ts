4d7c30aaea77a872eb1037e70611d8f85202223c
abdul rakib <abrakib94@gmail.com>
2024-01-18 17:18:17 +0600

    husky added

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-// const express = require('express')
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
-const port = 3000;
+const app: Application = express()
 
-app.use(cors);
+app.use(cors)
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-export default app;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-import config from "./config/index";
-import app from "./app";
+import config from './config/index'
+import app from './app'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.databaseurl as string);
+    await mongoose.connect(config.databaseurl as string)
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
-    console.log("data base successfully connected");
+      console.log(`Example app listening on port ${config.port}`)
+    })
+    console.log('data base successfully connected')
   } catch (error) {
-    console.log("faild to run", error);
+    console.log('faild to run', error)
   }
 }
-bootstrap();
+bootstrap()
